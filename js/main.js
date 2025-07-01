01ffb13564b3200b7c6e24639e85878c10f4ddcb
SardorQobilov <sardorqobilov555@gmail.com>
2025-07-01 12:03:34 +0500

    work html finished

---
+++ js/main.js
@@ -0,0 +1,28 @@
+const swiper = new Swiper(".constructions-swiper", {
+  slidesPerView: 6,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: ".swiper-btn-next",
+    prevEl: ".swiper-btn-prev",
+  },
+});
+
+// portfolio-swiper
+const portfolioSwiper = new Swiper(".portfolio-swiper", {
+  slidesPerView: 4,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: ".portfolio-swiper .swiper-btn-next",
+    prevEl: ".portfolio-swiper .swiper-btn-prev",
+  },
+});
+
+// glazing-steps-swiper
+const glazingStepsSwiper = new Swiper(".glazing-steps-swiper", {
+  slidesPerView: 6,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: ".glazing-steps-swiper .swiper-btn-next",
+    prevEl: ".glazing-steps-swiper .swiper-btn-prev",
+  },
+});
