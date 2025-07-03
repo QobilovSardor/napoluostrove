20019c51072279c321a9d87a44b01731bb2b51d2
SardorQobilov <sardorqobilov555@gmail.com>
2025-07-03 10:21:29 +0500

    facade page finished

--- js/main.js
+++ js/main.js
@@ -37,6 +37,66 @@ const blogSwiper = new Swiper(".blog-swiper", {
   },
 });
 
+// accessories-swiper
+const accessoriesSwiper = new Swiper(".accessories-swiper", {
+  slidesPerView: 5,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: ".accessories .swiper-btn-next",
+    prevEl: ".accessories .swiper-btn-prev",
+  },
+});
+
+const ctx = document.getElementById('myChart').getContext('2d');
+if (ctx) {
+  const myChart = new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+      labels: [
+        'Размер и геометрическая форма',
+        'Количество сборок',
+        'Тип оглавления',
+        'Тип профиля',
+        'Вид строповка',
+        'Качество фурнитуры',
+        'Цвет профиля',
+        'Акссесуары, монтажные сетки НПД.'
+      ],
+      datasets: [{
+        data: [25, 15, 10, 20, 10, 5, 10, 5], // Approximate percentages based on visual estimation
+        backgroundColor: [
+          '#1E90FF',
+          '#FF4500',
+          '#D3D3D3',
+          '#1E90FF',
+          '#FF4500',
+          '#FFA500',
+          '#D3D3D3',
+          '#FF4500'
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+      plugins: {
+        legend: {
+          display: false // Hide default legend
+        }
+      }
+    }
+  });
+
+  // Generate dynamic legend
+  const legendContainer = document.getElementById('legendContainer');
+  const legendItems = myChart.data.labels.map((label, index) => {
+    const color = myChart.data.datasets[0].backgroundColor[index];
+    return `<div class="legend-item"><span class="legend-color" style="background-color: ${color};"></span>${label}</div>`;
+  });
+  legendContainer.innerHTML = legendItems.join('');
+}
+
 // try {
 //   window.addEventListener('DOMContentLoaded', function () {
 //     VK.init({ apiId: 111, onlyWidgets: true });
